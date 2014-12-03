5fd1cf97341163533c794d6d38b693195304d763
Gage Peterson <justgage@gmail.com>
2014-12-03 09:45:22 -0700

    Added existing code to git repo

---
+++ lib/budget.ex
@@ -0,0 +1,117 @@
+defmodule Budget do
+
+  @path "~/.budget/"
+  defp path do
+    Path.expand(@path)
+  end
+
+  defp filename(name) do
+    Path.expand(@path <> name <> ".bud")
+  end
+
+  defp init do
+    if File.exists?(path()) == false do
+      File.mkdir_p(path())
+    end
+  end
+
+  defp init_file(name) do
+    if File.exists?(filename(name)) == false do
+      File.touch(filename(name))
+    end
+  end
+
+  def load(name) do
+
+    init()
+    init_file(name)
+
+    case File.read(filename(name)) do
+      {:ok, contents} -> contents
+      {:error, err} -> err
+    end
+  end
+
+
+  def append(name, lines) do
+    init()
+    init_file(name)
+    File.write(filename(name), lines, [:append])
+  end
+
+  def add(name, amount, disc \\ "") do
+    line = Float.to_string(amount, decimals: 2) <> "\t" <> disc <> "\n";
+    append(name, line)
+  end
+
+  def print(name, what) do
+    init()
+    init_file(name)
+
+    {:ok, file} = File.open(filename(name), [:read])
+
+    case what do
+      :balence -> IO.puts sum_file(file)
+      #:recent  -> 
+        _   -> print(name, :balence)
+    end
+
+    File.close(file)
+  end
+
+
+  defp sum_file(file, sum \\ 0) do
+    case IO.read(file, :line) do
+      :eof -> sum
+      data -> sum + sum_file(file, extract_sum data)
+    end
+  end
+
+  defp extract_sum(line) do
+    [num | _] = String.split line, "\t", parts: 2
+
+    String.to_float num
+  end
+
+  def help do
+    IO.puts "Help: 
+    COMMANDS:
+    $ budget add name amount [details] (eg: $budget add gage 12 \"got paid\")
+      'adds an expense to the budget'
+
+    $ budget show name
+      'shows `name`s current balence'
+
+    $ budget log name
+      'shows a log of recent expenses from `name`'
+      
+    $ budget help 
+      'shows this help text'
+    "
+  end
+
+  defp add_parse(details) do
+    case details do
+     [name, amount, disc] -> add name, amount, disc
+     [name, amount]       -> add name, amount
+     _                    -> IO.puts "Wrong number of arguments, expecting `name amount [description]`"
+    end
+  end
+
+  defp parse(command, details) do
+    command = String.downcase(command)
+    case command do
+      "help" -> help
+      "add"  -> add_parse(details)
+      _      -> IO.puts "\"" <> command <> "\" is not a recognized command, type `$ budget help` to get a list of commands"
+    end
+  end
+
+  def main(args) do
+    IO.puts "Welcome to budget!" 
+    case args do
+      [head | tail] -> parse(head, tail)
+      []            -> help
+    end     
+  end
+end
---
+++ mix.exs
@@ -0,0 +1,35 @@
+defmodule Budget.Mixfile do
+  use Mix.Project
+
+  def project do
+    [app: :budget,
+     version: "0.0.1",
+     elixir: "~> 1.0",
+     escript: escript,
+     deps: deps]
+  end
+
+  # Configuration for the OTP application
+  #
+  # Type `mix help compile.app` for more information
+  def application do
+    [applications: [:mix]]
+  end
+
+  def escript do
+    [ main_module: Budget, embeded_elixir: true ]
+  end
+
+  # Dependencies can be Hex packages:
+  #
+  #   {:mydep, "~> 0.3.0"}
+  #
+  # Or git/path repositories:
+  #
+  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
+  #
+  # Type `mix help deps` for more examples and options
+  defp deps do
+    []
+  end
+end
